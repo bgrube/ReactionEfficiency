b4dfd22397586eb7ec9a1f375f6b3550c2f7240e
bgrube <bgrube@jlab.org>
2022-07-15 17:12:41 -0400

    Calculate efficiencies for beam-energy bins and improve fit
    Improve fit procedure by going to a 3-step approach. Do not use "I"
    option to improve speed.

--- doubleGaussianPol2.C
+++ doubleGaussianPol2.C
@@ -31,7 +31,7 @@ struct doubleGaussianPol2 {
 		const double p0     = pars[0];
 		const double p1     = pars[1];
 		const double p2     = pars[2];
-		const double A      = pars[3] * pars[3];            // ensure positiveness
+		const double A      = pars[3];
 		const double r      = sin(pars[4]) * sin(pars[4]);  // ensure [0, 1] range
 		const double sigma1 = pars[5];
 		const double sigma2 = pars[6];
