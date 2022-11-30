18795839e118b7f3052fe090b0ce0f56f62c5471
bgrube <bgrube@jlab.org>
2022-11-30 15:00:44 -0500

    Fix syntax issue.

--- doit.C
+++ doit.C
@@ -3,10 +3,11 @@ doit(Long64_t nmbEntries = TTree::kMaxEntries)
 {
 	gROOT->ProcessLine(".x ./Load_DSelector.C");
 	TFile* f = nullptr;
+
 	// f = TFile::Open("./volatile/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic/030730/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730_000.root");
 	f = TFile::Open("./volatile/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730.root");
-	pippippimpimpmiss__B1_T1_U1_Effic_Tree->Process("./DSelector_pippippimpimpmiss.C+", "", nmbEntries);
+	f->Get<TTree>("pippippimpimpmiss__B1_T1_U1_Effic_Tree")->Process("./DSelector_pippippimpimpmiss.C+", "", nmbEntries);
 
 	// f = TFile::Open("./volatile/batch02/tree_pippimpmiss__B1_T1_U1_Effic_030730.root");
-	// pippimpmiss__B1_T1_U1_Effic_Tree->Process("./DSelector_pippimpmiss.C+", "", nmbEntries);
+	// f->Get<TTree>("pippimpmiss__B1_T1_U1_Effic_Tree")->Process("./DSelector_pippimpmiss.C+", "", nmbEntries);
 }
