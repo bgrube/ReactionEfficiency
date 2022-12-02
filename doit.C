cf3542d422b81d83811a703bc962273d45088dec
bgrube <bgrube@jlab.org>
2022-12-02 14:08:51 -0500

    Allow to run DSelector using PROOF-Lite.

--- doit.C
+++ doit.C
@@ -1,13 +1,28 @@
+// #include "TProof.h"
+// #include "TProofDebug.h"
+
+
 void
-doit(Long64_t nmbEntries = TTree::kMaxEntries)
+doit(const Long64_t nmbEntries = TTree::kMaxEntries)
 {
-	gROOT->ProcessLine(".x ./Load_DSelector.C");
-	TFile* f = nullptr;
+	R__LOAD_LIBRARY(libDSelector.so)
 
-	// f = TFile::Open("./volatile/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic/030730/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730_000.root");
-	f = TFile::Open("./volatile/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730.root");
-	f->Get<TTree>("pippippimpimpmiss__B1_T1_U1_Effic_Tree")->Process("./DSelector_pippippimpimpmiss.C+", "", nmbEntries);
+	const size_t nmbProofThreads = 10;
+	gROOT->ProcessLine(".x $(ROOT_ANALYSIS_HOME)/scripts/Load_DSelector.C");
 
-	// f = TFile::Open("./volatile/batch02/tree_pippimpmiss__B1_T1_U1_Effic_030730.root");
-	// f->Get<TTree>("pippimpmiss__B1_T1_U1_Effic_Tree")->Process("./DSelector_pippimpmiss.C+", "", nmbEntries);
+	const TString treeName        = "pippippimpimpmiss__B1_T1_U1_Effic_Tree";
+	// const TString fileNamePattern = "./2017_01-ver04/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic/030730/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730_000.root";
+	// const TString fileNamePattern = "./2017_01-ver04/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730.root";
+	const TString fileNamePattern = "./tree_pippippimpimpmiss__B1_T1_U1_Effic_bggen_2017_01-ver03_batch01.root";
+	// const TString treeName        = "pippimpmiss__B1_T1_U1_Effic_Tree";
+	// const TString fileNamePattern = "./2017_01-ver04/batch02/tree_pippimpmiss__B1_T1_U1_Effic_030730.root";
+	const TString selectorName    = "./DSelector_pippippimpimpmiss.C+";
+
+	TChain* chain = new TChain(treeName);
+	chain->Add(fileNamePattern);
+	// run with proof
+	gEnv->SetValue("ProofLite.Sandbox", "$PWD/.proof/");
+	DPROOFLiteManager::Process_Chain(chain, selectorName.Data(), nmbProofThreads);
+	// run interactively
+	// chain->Process(selectorName, "", nmbEntries);
 }
