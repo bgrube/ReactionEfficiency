aae309a7b75c92b490374e00343cb32b201e7990
bgrube <bgrube@jlab.org>
2022-12-06 21:15:03 -0500

    Add missing-mass-squared overlay plots for bggen MC topologies.

--- plotBggenTopologies.py
+++ plotBggenTopologies.py
@@ -9,10 +9,9 @@ ROOT.gStyle.SetHistMinimumZero(True)
 ROOT.gROOT.LoadMacro("~/rootlogon.C")
 
 
-def plotTopologies(normalize = False):
+def plotTopologies(maxNmbTopologies = 10, normalize = False):
   inFileName = "pippippimpimpmiss.root"
   histBaseName = "MissingMassSquared/ThrownTopologies"
-  maxNmbTopologies = 10
   colors = {
     "Found"   : ROOT.kGreen + 2,
     "Missing" : ROOT.kRed + 1,
@@ -25,7 +24,7 @@ def plotTopologies(normalize = False):
   if normalize:
     for hist in hists.values():
       hist.Scale(100 / hist.Integral())
-  hStack = ROOT.THStack("hStack", ";;" + ("Fraction" if normalize else "Number") + " of Combos" + (" [%]" if normalize else ""))
+  hStack = ROOT.THStack("hStackTopologies", ";;" + ("Fraction" if normalize else "Number") + " of Combos (RF-subtracted)" + (" [%]" if normalize else ""))
   # get overall distribution
   hist = hists[""]
   hist.SetName("Overall" if normalize else "Total")
@@ -59,8 +58,41 @@ def plotTopologies(normalize = False):
     legendEntry = legend.AddEntry(ROOT.MakeNullPointer(ROOT.TObject), "    " + str(round(integralOtherTopos)) + ("%" if normalize else " Combos"), "")
     legendEntry.SetTextColor(colors[case])
   canv.SaveAs(".pdf")
+  return [bins[i]["topology"] for i in range(maxNmbTopologies)]  # return maxNmbTopologies largest topologies in overall distribution
 
+
+def plotMissingMassSquared(topologies):
+  inFileName = "pippippimpimpmiss.root"
+  histBaseName = "MissingMassSquared/MissingMassSquared"
+  rebinFactor = 100
+  colorOffset = 1
+
+  # get histograms
+  inFile = ROOT.TFile(inFileName)
+  cases = ["Found", "Missing", ""]
+  hists = {topology : {case : inFile.Get(histBaseName + ("_" + case if case != "" else "") + "__" + topology) for case in cases} for topology in topologies}
+
+  # overlay distributions for topologies
+  hStacks = {case : ROOT.THStack("hStackMissingMassSquared" + case, ("Overall" if case == "" else case) + ";;Number of Combos (RF-subtracted)") for case in cases}
+  for case in cases:
+    for i, topology in enumerate(topologies):
+      hist = hists[topology][case]
+      hist.SetName(topology)
+      hist.SetLineColor(i + colorOffset)
+      hist.Rebin(rebinFactor)
+      hStacks[case].Add(hist)
+    canv = ROOT.TCanvas("mm2_topologies" + ("_" + case if case != "" else ""))
+    hStacks[case].Draw("NOSTACK HIST")
+    hStacks[case].GetXaxis().SetTitle(hists[topologies[0]][case].GetXaxis().GetTitle())
+    hStacks[case].GetXaxis().SetTitleOffset(0)
+    print(hStacks[case].GetXaxis().GetTitle())
+    # add legend
+    canv.BuildLegend()
+    canv.Modified()
+    canv.Update()
+    canv.SaveAs(".pdf")
 
 if __name__ == "__main__":
-  plotTopologies(False)
-  plotTopologies(True)
+  topologies = plotTopologies(normalize = False)
+  plotTopologies(normalize = True)
+  plotMissingMassSquared(topologies)
