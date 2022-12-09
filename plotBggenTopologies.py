5e7e53566f49d2f8e15f635451a3f2ceb632d45d
bgrube <bgrube@jlab.org>
2022-12-09 18:01:31 -0500

    Improve legends. Improve output file names. Add total distribution to missing-mass-squared overlays. Fix x-axis title not showing.

--- plotBggenTopologies.py
+++ plotBggenTopologies.py
@@ -6,11 +6,10 @@ import ROOT
 
 ROOT.gROOT.SetBatch(True)
 ROOT.gStyle.SetHistMinimumZero(True)
-ROOT.gROOT.LoadMacro("~/rootlogon.C")
 
 
 def plotTopologies(maxNmbTopologies = 10, normalize = False):
-  inFileName = "pippippimpimpmiss.root"
+  inFileName = "pippippimpimpmiss_bggen_2017_01-ver03.root"
   histBaseName = "MissingMassSquared/ThrownTopologies"
   colors = {
     "Found"   : ROOT.kGreen + 2,
@@ -43,14 +42,16 @@ def plotTopologies(maxNmbTopologies = 10, normalize = False):
       assert bin["topology"] == hist.GetXaxis().GetBinLabel(index + 1)
     hist.SetLineColor(colors[case])
     hStack.Add(hist)
+    print(f"{case} signal: {hist.GetBinContent(1)}{'%' if normalize else ' combos'}")
   hStack.Add(hists[""])  # draw on top
+  print(f"Overall signal: {hists[''].GetBinContent(1)}{'%' if normalize else ' combos'}")
   # plot overall distribution
-  canv = ROOT.TCanvas("topologies" + ("_norm" if normalize else ""))
+  canv = ROOT.TCanvas("justin_Proton_4pi_topologies" + ("_norm" if normalize else ""))
   hStack.Draw("NOSTACK HIST")
   hStack.GetXaxis().SetRangeUser(0, maxNmbTopologies)
   hStack.SetMinimum(0)
   # add legend
-  legend = canv.BuildLegend()
+  legend = canv.BuildLegend(0.7, 0.65, 0.99, 0.99)
   # add labels that show number or fraction outside of plot range
   legend.AddEntry(ROOT.MakeNullPointer(ROOT.TObject), "Other topologies:", "")
   for case in cases:
@@ -62,7 +63,7 @@ def plotTopologies(maxNmbTopologies = 10, normalize = False):
 
 
 def plotMissingMassSquared(topologies):
-  inFileName = "pippippimpimpmiss.root"
+  inFileName = "pippippimpimpmiss_bggen_2017_01-ver03.root"
   histBaseName = "MissingMassSquared/MissingMassSquared"
   rebinFactor = 100
   colorOffset = 1
@@ -71,28 +72,45 @@ def plotMissingMassSquared(topologies):
   inFile = ROOT.TFile(inFileName)
   cases = ["Found", "Missing", ""]
   hists = {topology : {case : inFile.Get(histBaseName + ("_" + case if case != "" else "") + "__" + topology) for case in cases} for topology in topologies}
+  histsTotal = {case : inFile.Get(histBaseName + ("_" + case if case != "" else "")) for case in cases}
+  print(histsTotal)
 
   # overlay distributions for topologies
-  hStacks = {case : ROOT.THStack("hStackMissingMassSquared" + case, ("Overall" if case == "" else case) + ";;Number of Combos (RF-subtracted)") for case in cases}
+  hStacks = {case : ROOT.THStack("hStackMissingMassSquared" + case, ("Overall" if case == "" else case) + f";{hists[topologies[0]][case].GetXaxis().GetTitle()};Number of Combos (RF-subtracted)") for case in cases}
   for case in cases:
+    # total distribution
+    hist = histsTotal[case]
+    hist.SetName("Total")
+    hist.Rebin(rebinFactor)
+    hist.SetFillColor(ROOT.kGray)
+    hist.SetLineColor(ROOT.kGray)
+    hStacks[case].Add(hist)
+    # distribution for topologies
     for i, topology in enumerate(topologies):
       hist = hists[topology][case]
       hist.SetName(topology)
       hist.SetLineColor(i + colorOffset)
       hist.Rebin(rebinFactor)
       hStacks[case].Add(hist)
-    canv = ROOT.TCanvas("mm2_topologies" + ("_" + case if case != "" else ""))
+    canv = ROOT.TCanvas("justin_Proton_4pi_mm2_bggen_topologies" + ("_" + case if case != "" else ""))
     hStacks[case].Draw("NOSTACK HIST")
-    hStacks[case].GetXaxis().SetTitle(hists[topologies[0]][case].GetXaxis().GetTitle())
-    hStacks[case].GetXaxis().SetTitleOffset(0)
-    print(hStacks[case].GetXaxis().GetTitle())
     # add legend
-    canv.BuildLegend()
-    canv.Modified()
-    canv.Update()
+    canv.BuildLegend(0.7, 0.65, 0.99, 0.99)
     canv.SaveAs(".pdf")
 
 if __name__ == "__main__":
+  ROOT.gROOT.LoadMacro("~/rootlogon.C")
+  ROOT.gROOT.ForceStyle()
+  ROOT.gStyle.SetCanvasDefW(600)
+  ROOT.gStyle.SetCanvasDefH(400)
+  ROOT.gStyle.SetPalette(ROOT.kBird)
+  ROOT.gStyle.SetLegendFillColor(ROOT.kWhite)
+  ROOT.gStyle.SetLegendBorderSize(1)
+  # ROOT.gStyle.SetOptStat("ni")  # show only name and integral
+  ROOT.gStyle.SetOptStat("i")  # show only integral
+  ROOT.gStyle.SetStatFormat("8.8g")
+  ROOT.gStyle.SetTitleColor(1, "X")  # fix that for some mysterious reason x-axis titles of 2D plots and graphs are white
+
   topologies = plotTopologies(normalize = False)
   plotTopologies(normalize = True)
   plotMissingMassSquared(topologies)
