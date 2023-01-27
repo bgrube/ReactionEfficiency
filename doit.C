7c004b0fedebce7f682f4027a7639af3c43bfbfb
bgrube <bgrube@jlab.org>
2023-01-26 19:04:10 -0500

    Optimize parallelism.

--- doit.C
+++ doit.C
@@ -6,20 +6,20 @@ doit(
 {
 	R__LOAD_LIBRARY(libDSelector.so)
 
-	const size_t nmbProofThreads = 5;
+	const size_t nmbProofThreads = 10;
 	gROOT->ProcessLine(".x $(ROOT_ANALYSIS_HOME)/scripts/Load_DSelector.C");
 
 	const TString selectorName    = "./DSelector_pippippimpimpmiss.C+";
 	// pi+pi+pi-pi-(p)
 	const TString treeName        = "pippippimpimpmiss__B1_T1_U1_Effic_Tree";
-	// const TString fileNamePattern = "./2017_01-ver04/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic/030730/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730_000.root";
-	// const TString fileNamePattern = "./2017_01-ver04/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730.root";
-	// const TString fileNamePattern = "./bggen_2017_01-ver03/batch01/tree_pippippimpimpmiss__B1_T1_U1_Effic/030285/tree_pippippimpimpmiss__B1_T1_U1_Effic_030285_000.root";
-	const TString fileNamePattern = "./tree_pippippimpimpmiss__B1_T1_U1_Effic_bggen_2017_01-ver03_batch01.root";
+	// const TString fileNamePattern = "./data/2017_01-ver04/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic/030730/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730_000.root";
+	const TString fileNamePattern = "./data/2017_01-ver04/batch02/tree_pippippimpimpmiss__B1_T1_U1_Effic_030730.root";
+	// const TString fileNamePattern = "./data/bggen_2017_01-ver03/batch01/tree_pippippimpimpmiss__B1_T1_U1_Effic/030285/tree_pippippimpimpmiss__B1_T1_U1_Effic_030285_000.root";
+	// const TString fileNamePattern = "./data/tree_pippippimpimpmiss__B1_T1_U1_Effic_bggen_2017_01-ver03_batch01.root";
 	// pi+pi-(p)
 	// const TString selectorName    = "./DSelector_pippimpmiss.C+";
 	// const TString treeName        = "pippimpmiss__B1_T1_U1_Effic_Tree";
-	// const TString fileNamePattern = "./2017_01-ver04/batch02/tree_pippimpmiss__B1_T1_U1_Effic_030730.root";
+	// const TString fileNamePattern = "./data/2017_01-ver04/batch02/tree_pippimpmiss__B1_T1_U1_Effic_030730.root";
 
 	TChain* chain = new TChain(treeName);
 	chain->Add(fileNamePattern);
