12849a84d7143cfd2a0d616735ec4001d927e757
bgrube <bgrube@jlab.org>
2023-03-31 15:26:03 -0400

    Add error checking.

--- doit.sh
+++ doit.sh
@@ -5,6 +5,12 @@ FIT_DIR="./BruFitOutput"
 rm -rf "${FIT_DIR}"
 mkdir -p "${FIT_DIR}"
 ./fitMissingMassSquared.py "${FIT_DIR}" &> "${FIT_DIR}/fitMissingMassSquared.log"
+RET=${?}
+if [ ${RET} -ne 0 ]
+then
+  echo "Fitting script failed with exit code '${RET}'; exiting"
+  exit 1
+fi
 
 ./cleanFitDir.sh "${FIT_DIR}"
 ./cleanBruFitLogFile.py "${FIT_DIR}"
