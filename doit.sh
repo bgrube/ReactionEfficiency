8963434d7b9c2c6e39135c12534f752e581d61fe
bgrube <bgrube@jlab.org>
2023-03-30 21:20:26 -0400

    Fix mistakes.

--- doit.sh
+++ doit.sh
@@ -3,7 +3,8 @@
 FIT_DIR="./BruFitOutput"
 
 rm -rf "${FIT_DIR}"
-./fitMissingMassSquared.py &> "${FIT_DIR}/fitMissingMassSquared.log"
+mkdir -p "${FIT_DIR}"
+./fitMissingMassSquared.py "${FIT_DIR}" &> "${FIT_DIR}/fitMissingMassSquared.log"
 
 ./cleanFitDir.sh "${FIT_DIR}"
 ./cleanBruFitLogFile.py "${FIT_DIR}"
