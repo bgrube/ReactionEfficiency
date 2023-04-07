b85853c2aca752f0df6bca2842731b1a30167a43
bgrube <bgrube@jlab.org>
2023-04-06 20:33:17 -0400

    Disable command-line interface. Print git repo info. Use colorblind-friendly style switcher.

--- overlayEfficiencies.py
+++ overlayEfficiencies.py
@@ -61,23 +61,19 @@ def overlayEfficiencies(
   pdfFileNameSuffix: str   = "",
   particle:          str   = "Proton",
   channel:           str   = "4pi",
-  markerSize:        float = 0.75,
 ):
   '''Overlays efficiencies as a function of `binningVar` for all given fits'''
   print(f"Overlaying efficiencies for binning variable '{binningVar}'")
   efficiencyMultiGraph = ROOT.TMultiGraph()  # type: ignore
   efficiencyGraphs = {}  # store graphs here to keep them in memory
   shiftFraction = 0
-  color = 1
+  styleIndex = 1
   for (fitResultDirName, fitLabel), efficiencies in effInfos.items():
     graph = efficiencyGraphs[fitResultDirName] = plotFitResults.getParValueGraph1D(plotEfficiencies.getEffValuesForGraph1D(binningVar, efficiencies), shiftFraction)
-    graph.SetTitle(fitLabel)            # type: ignore
-    graph.SetMarkerStyle(ROOT.kCircle)  # type: ignore
-    graph.SetMarkerSize(markerSize)     # type: ignore
-    graph.SetMarkerColor(color)         # type: ignore
-    graph.SetLineColor(color)           # type: ignore
     shiftFraction += 0.01
-    color += 1
+    graph.SetTitle(fitLabel)                                            # type: ignore
+    makePlots.setCbFriendlyStyle(graph, styleIndex)
+    styleIndex += 1
     efficiencyMultiGraph.Add(graph)
   efficiencyMultiGraph.SetTitle(f"{particle} Track-Finding Efficiency ({channel})")
   assert binningVar in BINNING_VAR_PLOT_INFO, f"No plot information for binning variable '{binningVar}'"
@@ -93,23 +89,36 @@ def overlayEfficiencies(
 
 
 if __name__ == "__main__":
+  makePlots.printGitInfo()
   ROOT.gROOT.SetBatch(True)  # type: ignore
   makePlots.setupPlotStyle()
   ROOT.gROOT.ProcessLine(f".x {os.environ['BRUFIT']}/macros/LoadBru.C")  # type: ignore
 
-  # echo and parse command line
-  print(f"Script was called using: '{' '.join(sys.argv)}'")
-  parser = argparse.ArgumentParser(description="Overlays efficiency graphs obtained from BruFit results in given directories.")
-  parser.add_argument("fitResultDirNames", type = str, nargs = "+", default = ["./BruFitOutput"], help = "The paths to the BruFit output directories that should be overlaid; (default: %(default)s)")
-  parser.add_argument("--fitLabels", type = str, nargs = "*", default = [], help = "The legend labels for each fit (same order as directory names); (default: directory names)")
-  args = parser.parse_args()
-  # fitVariable = "MissingMassSquared_Measured"
+  # # echo and parse command line
+  # print(f"Script was called using: '{' '.join(sys.argv)}'")
+  # parser = argparse.ArgumentParser(description="Overlays efficiency graphs obtained from BruFit results in given directories.")
+  # parser.add_argument("fitResultDirNames", type = str, nargs = "+", default = ["./BruFitOutput"], help = "The paths to the BruFit output directories that should be overlaid; (default: %(default)s)")
+  # parser.add_argument("--fitLabels", type = str, nargs = "*", default = [], help = "The legend labels for each fit (same order as directory names); (default: directory names)")
+  # args = parser.parse_args()
 
-  fitResultDirNames = ["./BruFitOutput.data_allFixed", "./BruFitOutput.data_bkgAllFudge", "./BruFitOutput.data_allFudge"]
-  fitLabels         = ["All fixed", "Sig fixed", "All free"]
+  fitResults = [
+    # no extra cuts
+    ("./noCut/BruFitOutput.sig_fixed",                     "Truth"),
+    ("./noCut/BruFitOutput.bggen_allFixed",                "bggen all fixed"),
+    # ("./noCut/BruFitOutput.bggen_sigAllFixed_bkgAllFudge", "bggen sig fixed"),
+    # ("./noCut/BruFitOutput.bggen_allFudge",                "bggen all free"),
+    ("./noCut/BruFitOutput.data_allFixed",                 "Data all fixed"),
+    # ("./noCut/BruFitOutput.data_bkgAllFudge",              "Data sig fixed"),
+    # ("./noCut/BruFitOutput.data_allFudge",                 "Data all free"),
+    # no unused showers
+    ("./noShowers/BruFitOutput.bggen_noShowers_allFixed",   "bggen NS all fixed"),
+    ("./noShowers/BruFitOutput.data_noShowers_allFixed",    "Data NS all fixed"),
+    # ("./noShowers/BruFitOutput.data_noShowers_bkgAllFudge", "Data NS sig fixed"),
+  ]
+  fitResultDirNames = tuple(fitResult[0] for fitResult in fitResults)
+  fitLabels         = tuple(fitResult[1] for fitResult in fitResults)
   effInfos, binVarNames = getEfficiencies(fitResultDirNames, fitLabels)
   print("Overlaying efficiencies")
-  print(effInfos)
   if effInfos:
     if binVarNames:
       for binningVars in binVarNames:
