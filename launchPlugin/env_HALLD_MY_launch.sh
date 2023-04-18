4da86717539e056a289f622d01f28f129fbfa228
bgrube <bgrube@jlab.org>
2023-04-18 13:38:06 -0400

    Switch to latest version of software stack.

--- launchPlugin/env_HALLD_MY_launch.sh
+++ launchPlugin/env_HALLD_MY_launch.sh
@@ -3,7 +3,7 @@
 # ALTERNATIVE HOME FOLDER
 export HALLD_MY="/work/halld/bgrube/halld_my"
 # SET FULL ENVIRONMENT
-source /group/halld/Software/build_scripts/gluex_env_jlab.sh /group/halld/www/halldweb/html/halld_versions/version_5.7.1.xml
+source /group/halld/Software/build_scripts/gluex_env_jlab.sh /group/halld/www/halldweb/html/halld_versions/version_5.10.0.xml
 
 # # quick fix for rootcling compilation
 # unset CPLUS_INCLUDE_PATH
