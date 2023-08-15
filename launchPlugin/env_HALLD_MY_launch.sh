2a9f92fdbc5d83ce513e1b5ea85f1db4e90ecba6
bgrube <bgrube@jlab.org>
2023-08-15 18:09:32 -0400

    Use local halld_recon in cluster jobs.

--- launchPlugin/env_HALLD_MY_launch.sh
+++ launchPlugin/env_HALLD_MY_launch.sh
@@ -3,7 +3,9 @@
 # ALTERNATIVE HOME FOLDER
 export HALLD_MY="/work/halld/bgrube/halld_my"
 # SET FULL ENVIRONMENT
-source /group/halld/Software/build_scripts/gluex_env_jlab.sh /group/halld/www/halldweb/html/halld_versions/version_5.10.0.xml
+# source /group/halld/Software/build_scripts/gluex_env_jlab.sh /group/halld/www/halldweb/html/halld_versions/version_5.11.0.xml
+# use private copy of halld-recon
+source /group/halld/Software/build_scripts/gluex_env_jlab.sh /work/halld/bgrube/ProtonTrackEfficiency/ReactionEfficiency/launchPlugin/version.xml.halld_recon
 
 # # quick fix for rootcling compilation
 # unset CPLUS_INCLUDE_PATH
