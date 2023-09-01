bc5214aedce2dc3b7dc26320e48398ad5a092b35
bgrube <bgrube@jlab.org>
2023-09-01 15:18:44 -0400

    Implement single source for tree name.

--- runSelector.py
+++ runSelector.py
@@ -54,32 +54,32 @@ if __name__ == "__main__":
 
   # pi+pi+pi-pi-(p)
   selectorFileName = "./DSelector_pippippimpimpmiss.C"
-  # treeName = "pippippimpimpmiss__B1_T1_U1_Effic_Tree"
-  # # dataSets = [{"type" : "MCbggen", "period" : "2017_01-ver03"}]
+  # treeBaseName = "pippippimpimpmiss__B1_T1_U1_Effic"
+  treeBaseName = "pippippimpimmissprot__B1_T1_U1_Effic"
+  # dataSets = [{"type" : "MCbggen", "period" : "2017_01-ver03"}]
+  dataSets = [{"type" : "MCbggen", "period" : "2018_01-ver02"}]
   # dataSets = [{"type" : "RD",      "period" : "2018_01-ver02", "run" : "041003"}]
-  # # dataSets = [{"type" : "RD",      "period" : "2018_01-ver02", "run" : "042030"}]
-  # # dataSets = [{"type" : "RD",      "period" : "2018_01-ver02", "run" : "042550"}]
-  # # dataSets = [{"type" : "MCbggen", "period" : "2018_01-ver02"}]
-  # for dataSet in dataSets:
-  #   dataSet.update({"fileName" : f"./data/{dataSet['type']}/{dataSet['period']}/tree_pippippimpimpmiss__B1_T1_U1_Effic_{dataSet['type']}_{dataSet['period']}"
-  #   + (f"_{dataSet['run']}" if "run" in dataSet else "") + ".root"})
-  treeName = "pippippimpimmissprot__B1_T1_U1_Effic_Tree"
+  # dataSets = [{"type" : "RD",      "period" : "2018_01-ver02", "run" : "042030"}]
+  # dataSets = [{"type" : "RD",      "period" : "2018_01-ver02", "run" : "042550"}]
+  for dataSet in dataSets:
+    dataSet.update({"fileName" : f"./data/{dataSet['type']}/{dataSet['period']}/tree_{treeBaseName}_{dataSet['type']}_{dataSet['period']}"
+      + (f"_{dataSet['run']}" if "run" in dataSet else "") + ".root"})
   # inFiles = sorted(glob.glob("./data/RD/2019_11-ver01/*.root"))
-  inFiles = ["./data/RD/2019_11-ver01/tree_pippippimpimmissprot__B1_T1_U1_Effic_071592.root"]
-  dataSets = []
-  for inFile in inFiles:
-    runNumber = inFile.split(".")[-2].split("_")[-1]  # extract run number from file name of the form `tree_pippippimpimmissprot__B1_T1_U1_Effic_<run number>.root`
-    dataSet = {"type" : "RD", "period" : "2019_11-ver01", "run" : runNumber}
-    dataSet.update({"fileName" : f"./data/{dataSet['type']}/{dataSet['period']}/tree_pippippimpimmissprot__B1_T1_U1_Effic_{dataSet['run']}.root"})
-    dataSets.append(dataSet)
+  # inFiles = [f"./data/RD/2019_11-ver01/tree_{treeBaseName}_071592.root"]
+  # dataSets = []
+  # for inFile in inFiles:
+  #   runNumber = inFile.split(".")[-2].split("_")[-1]  # extract run number from file name of the form `tree_{treeBaseName}_<run number>.root`
+  #   dataSet = {"type" : "RD", "period" : "2019_11-ver01", "run" : runNumber}
+  #   dataSet.update({"fileName" : f"./data/{dataSet['type']}/{dataSet['period']}/tree_{treeBaseName}_{dataSet['run']}.root"})
+  #   dataSets.append(dataSet)
   # pi+pi-(p)
   # selectorFileName = "./DSelector_pippimpmiss.C"
-  # treeName = "pippimpmiss__B1_T1_U1_Effic_Tree"
+  # treeBaseName = "pippimpmiss__B1_T1_U1_Effic_Tree"
 
   # run selector and create flat-tree file
   flatTreeFileNames: List[str] = []
   for dataSet in dataSets:
-    runSelector(dataSet["fileName"], treeName, selectorFileName)
+    runSelector(dataSet["fileName"], f"{treeBaseName}_Tree", selectorFileName)
     # rename output files
     channel = "pippippimpimpmiss"
     histFileName = f"./{channel}.{dataSet['type']}_{dataSet['period']}" + (f"_{dataSet['run']}" if "run" in dataSet else "") + ".root"
