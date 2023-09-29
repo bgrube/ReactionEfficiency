feee50ca43ae8dbf9719d505dfc74503d6ca6d45
bgrube <bgrube@jlab.org>
2023-09-29 11:53:03 -0400

    Generalize amd simplify code that runs DSelector and produces BruFit files.

--- runSelector.py
+++ runSelector.py
@@ -52,51 +52,46 @@ if __name__ == "__main__":
 
   deleteFlatTreeFiles = False
 
+  # define channel
   # pi+pi+pi-pi-(p)
-  selectorFileName = "./DSelector_pippippimpimpmiss.C"
-  # treeBaseName = "pippippimpimpmiss__B1_T1_U1_Effic"
-  treeBaseName = "pippippimpimmissprot__B1_T1_U1_Effic"
-  # dataSets = [{"type" : "MCbggen", "period" : "2017_01-ver03"}]
-  # dataSets = [{"type" : "MCbggen", "period" : "2018_01-ver02"}]
-  # dataSets = [{"type" : "RD",      "period" : "2018_01-ver02", "run" : "041003"}]
-  # dataSets = [{"type" : "RD",      "period" : "2018_01-ver02", "run" : "042030"}]
-  # dataSets = [{"type" : "RD",      "period" : "2018_01-ver02", "run" : "042550"}]
-  # for dataSet in dataSets:
-  #   dataSet.update({"fileName" : f"./data/{dataSet['type']}/{dataSet['period']}/tree_{treeBaseName}_{dataSet['type']}_{dataSet['period']}"
-  #     + (f"_{dataSet['run']}" if "run" in dataSet else "") + ".root"})
-  period = "2018_01-ver02"
-  # period = "2019_11-ver01"
-  inFileNames = sorted(glob.glob(f"./data/RD/{period}/tree_{treeBaseName}_RD_{period}_??????.root"))
-  # inFiles = [f"./data/RD/2019_11-ver01/tree_{treeBaseName}_071592.root"]
-  dataSets = []
-  for inFile in inFileNames:
-    runNumber = inFile.split(".")[-2].split("_")[-1]  # extract run number from file name of the form `tree_{treeBaseName}_<run number>.root`
-    dataSet = {"type" : "RD", "period" : period, "run" : runNumber}
-    dataSet.update({"fileName" : f"./data/{dataSet['type']}/{dataSet['period']}/tree_{treeBaseName}_{dataSet['type']}_{dataSet['period']}_{dataSet['run']}.root"})
-    dataSets.append(dataSet)
+  channel = "pippippimpimpmiss"  # used for output tree and file names
+  treeName = "pippippimpimmissprot__B1_T1_U1_Effic"
   # pi+pi-(p)
-  # selectorFileName = "./DSelector_pippimpmiss.C"
-  # treeBaseName = "pippimpmiss__B1_T1_U1_Effic_Tree"
+  channel = "pippimpmiss"
+  # treeName = "pippimpmiss__B1_T1_U1_Effic"
+  selectorFileName = f"./DSelector_{channel}.C"
+
+  # define data
+  # dataType = "MCbggen"
+  dataType = "RD"
+  # dataPeriod = "2017_01-ver03"
+  # dataPeriod = "2018_01-ver02"
+  dataPeriod = "2018_08-ver02"
+  # dataPeriod = "2019_11-ver01"
+  dataDir = f"./data/{dataType}/{dataPeriod}"
+  inFileNames = sorted(glob.glob(f"{dataDir}/tree_{treeName}_{dataType}_{dataPeriod}*.root"))
 
   # run selector and create flat-tree file
-  channel = "pippippimpimpmiss"  # used for output tree and file names
   flatTreeFileNames: List[str] = []
-  for dataSet in dataSets:
-    runSelector(dataSet["fileName"], f"{treeBaseName}_Tree", selectorFileName)
+  for inFileName in inFileNames:
+    runNumber = inFileName.split(".")[-2].split("_")[-1]  # extract run number from file name of the form `tree_{treeName}_<run number>.root`
+    if not runNumber.isnumeric():
+      runNumber = None
+    # runSelector(inFileName, f"{treeName}_Tree", selectorFileName)
     # rename output files
-    histFileName = f"./data/{dataSet['type']}/{dataSet['period']}/{channel}.{dataSet['type']}_{dataSet['period']}" + (f"_{dataSet['run']}" if "run" in dataSet else "") + ".root"
+    histFileName = f"{dataDir}/{channel}.{dataType}_{dataPeriod}" + ("" if runNumber is None else f"_{runNumber}") + ".root"
     print(f"Moving histogram file to '{histFileName}'")
-    os.replace(f"{channel}.root", histFileName)
-    flatTreeFileName = f"./data/{dataSet['type']}/{dataSet['period']}/{channel}_flatTree.{dataSet['type']}_{dataSet['period']}" + (f"_{dataSet['run']}" if "run" in dataSet else "") + ".root"
+    # os.replace(f"{channel}.root", histFileName)
+    flatTreeFileName = f"{dataDir}/{channel}_flatTree.{dataType}_{dataPeriod}" + ("" if runNumber is None else f"_{runNumber}") + ".root"
     print(f"Moving flat-tree file to '{flatTreeFileName}'")
-    os.replace(f"{channel}_flatTree.root", flatTreeFileName)
+    # os.replace(f"{channel}_flatTree.root", flatTreeFileName)
     flatTreeFileNames.append(flatTreeFileName)
     print()
 
   # convert flat tree to BruFit tree
   for flatTreeFileName in flatTreeFileNames:
     bruFitTreeFileName = f"{flatTreeFileName}.brufit"
-    makeBruFitTree.makeBruFitTree(flatTreeFileName, outputFileName = bruFitTreeFileName)
+    # makeBruFitTree.makeBruFitTree(flatTreeFileName, outputFileName = bruFitTreeFileName)
     if deleteFlatTreeFiles:
       print(f"Removing flat-tree file '{flatTreeFileName}'")
-      os.remove(flatTreeFileName)
+      # os.remove(flatTreeFileName)
