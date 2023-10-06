5f26670e3fc31f034a72519f870e6550b4f647ec
bgrube <bgrube@jlab.org>
2023-10-06 12:17:08 -0400

    Add Spring 2017 data to scripts that handle run lists.

--- launchPlugin/makeJcacheFileList.sh
+++ launchPlugin/makeJcacheFileList.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
+MSS_DIR="/mss/halld/RunPeriod-2017-01/recon/ver03/REST"
+RUN_LIST="2017_01-ver03.part.runList"
 # MSS_DIR="/mss/halld/RunPeriod-2018-01/recon/ver02/REST"
 # RUN_LIST="2018_01-ver02.part.runList"
-MSS_DIR="/mss/halld/RunPeriod-2018-08/recon/ver02/REST"
-RUN_LIST="2018_08-ver02.part.runList"
+# MSS_DIR="/mss/halld/RunPeriod-2018-08/recon/ver02/REST"
+# RUN_LIST="2018_08-ver02.part.runList"
 
 CMD_LINE="makeJcacheFileList.py"
 while read RUN_NMB; do
--- launchPlugin/makeRunList.sh
+++ launchPlugin/makeRunList.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/bash
 
+MSS_DIR="/mss/halld/RunPeriod-2017-01/recon/ver03/REST"
+RUN_LIST="2018_01-ver02.runList"
 # MSS_DIR="/mss/halld/RunPeriod-2018-01/recon/ver02/REST"
 # RUN_LIST="2018_01-ver02.runList"
-MSS_DIR="/mss/halld/RunPeriod-2018-08/recon/ver02/REST"
-RUN_LIST="2018_08-ver02.runList"
+# MSS_DIR="/mss/halld/RunPeriod-2018-08/recon/ver02/REST"
+# RUN_LIST="2018_08-ver02.runList"
 
 rm -f "${RUN_LIST}"
 for RUN in $(find "${MSS_DIR}" -type d -name '??????')
 do
-  echo $(basename "${RUN}") >> "${RUN_LIST}"
+  basename "${RUN}" >> "${RUN_LIST}"
 done
 
 # see https://unix.stackexchange.com/a/369204
