1aeac7f64412211deec379d42f29947ebd42bb17
bgrube <bgrube@jlab.org>
2023-10-06 12:35:20 -0400

    Add Spring 2020 data to run-list script.

--- launchPlugin/makeRunList.sh
+++ launchPlugin/makeRunList.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
-MSS_DIR="/mss/halld/RunPeriod-2017-01/recon/ver03/REST"
-RUN_LIST="2017_01-ver03.runList"
+# MSS_DIR="/mss/halld/RunPeriod-2017-01/recon/ver03/REST"
+# RUN_LIST="2017_01-ver03.runList"
 # MSS_DIR="/mss/halld/RunPeriod-2018-01/recon/ver02/REST"
 # RUN_LIST="2018_01-ver02.runList"
 # MSS_DIR="/mss/halld/RunPeriod-2018-08/recon/ver02/REST"
 # RUN_LIST="2018_08-ver02.runList"
+MSS_DIR="/mss/halld/RunPeriod-2019-11/recon/ver01/REST"
+RUN_LIST="2019_11-ver01.runList"
 
 rm -f "${RUN_LIST}"
 for RUN in $(find "${MSS_DIR}" -type d -name '??????')
