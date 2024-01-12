d7a7822110591209e1cbbf8f7cd671b348218378
bgrube <bgrube@jlab.org>
2024-01-12 12:11:59 -0500

    Fix bug that data period was not properly propagated to output path. Improve naming. Add TODO.

--- runFits.py
+++ runFits.py
@@ -31,11 +31,13 @@ if __name__ == "__main__":
       { # bggen MC
         **dict.fromkeys(["dataFileName", "bggenFileName"],
                         f"./data/MCbggen/{dataPeriod}/pippippimpimpmiss_flatTree.MCbggen_{dataPeriod}.root.brufit"),  # dataFileName and bggenFileName have identical value
-        "dataLabel" : f"bggen_{dataPeriod}",
+        "dataPeriod" : dataPeriod,
+        "dataLabel"  : f"bggen_{dataPeriod}",
       },
       { # real data
         "dataFileName"  : f"./data/RD/{dataPeriod}/pippippimpimpmiss_flatTree.RD_{dataPeriod}.root.brufit",
         "bggenFileName" : f"./data/MCbggen/{dataPeriod}/pippippimpimpmiss_flatTree.MCbggen_{dataPeriod}.root.brufit",
+        "dataPeriod"    : dataPeriod,
         "dataLabel"     : f"data_{dataPeriod}",
       },
     ]
@@ -44,7 +46,7 @@ if __name__ == "__main__":
     # {
     #   "fitDirectory" : f"BruFitOutput.{dataSample['dataLabel']}_sigAllFixed_noBkg",
     #   "pdfTypeSig" : "Histogram", "fixParsSig" : "smear shift scale",
-    #   "pdfTypeBkg" : "\"\"",
+    #   "pdfTypeBkg" : "\"\"",  #TODO is it correct to define a string '""'? why not just empty string?
     # },
     # {
     #   "fitDirectory" : f"BruFitOutput.{dataSample['dataLabel']}_sigAllFudge_noBkg",
@@ -132,11 +134,12 @@ if __name__ == "__main__":
     #   "pdfTypeBkg" : "Histogram",
     # },
   ] for dataSample in dataSamples]
-  # add input files (same for all studies)
+  # add data period and input files (same for all fits of a given data sample)
   for index, dataSample in enumerate(dataSamples):
-    for study in fits[index]:
-      study.update({"dataFileName"  : dataSample["dataFileName"]})
-      study.update({"bggenFileName" : dataSample["bggenFileName"]})
+    for fit in fits[index]:
+      fit.update({"dataPeriod"    : dataSample["dataPeriod"]})
+      fit.update({"dataFileName"  : dataSample["dataFileName"]})
+      fit.update({"bggenFileName" : dataSample["bggenFileName"]})
 
   # pdfTypeBkg = "DoubleGaussian",
   # pdfTypeBkg = "DoubleGaussian_SameMean",
@@ -144,10 +147,9 @@ if __name__ == "__main__":
   # pdfTypeBkg = "SkewedGaussian_ExpMod",
   # pdfTypeBkg = "SkewedGaussian_Log",
 
-  for dataSamples in fits:
-    for study in dataSamples:
-      # print(f"!!! {study}")
-      fitDirectory = f"./fits/{dataPeriod}/noShowers/{study['fitDirectory']}"
+  for fitsForDataSample in fits:
+    for fit in fitsForDataSample:
+      fitDirectory = f"./fits/{fit['dataPeriod']}/noShowers/{fit['fitDirectory']}"
       # prepare directories
       shutil.rmtree(fitDirectory, ignore_errors = True)
       os.makedirs(fitDirectory, exist_ok = True)
@@ -155,9 +157,9 @@ if __name__ == "__main__":
       # run fits
       #TODO call python functions directly instead of making the detour via the command-line interface
       print(f"Starting fits ...")
-      cmdLineOptions = [(f"--{option} {study[option]}" if option in study else "") for option in ("pdfTypeSig", "fixParsSig", "pdfTypeBkg", "fixParsBkg")]
+      cmdLineOptions = [(f"--{option} {fit[option]}" if option in fit else "") for option in ("pdfTypeSig", "fixParsSig", "pdfTypeBkg", "fixParsBkg")]
       result = subprocess.run(
-        f"./fitMissingMassSquared.py \"{fitDirectory}\" {study['dataFileName']} {study['bggenFileName']} {' '.join(cmdLineOptions)} &> \"{fitDirectory}/fitMissingMassSquared.log\"", shell = True)
+        f"./fitMissingMassSquared.py \"{fitDirectory}\" {fit['dataFileName']} {fit['bggenFileName']} {' '.join(cmdLineOptions)} &> \"{fitDirectory}/fitMissingMassSquared.log\"", shell = True)
       if result.returncode != 0:
         raise RuntimeError(f"Fitting script failed with exit code '{result.returncode}'")
       # postprocess fit results
