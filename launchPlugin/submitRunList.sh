7192b363134bd2d6b814975eaea85bc056bb2a0e
bgrube <bgrube@jlab.org>
2024-09-30 14:11:55 -0400

    Adapt paths.

--- launchPlugin/submitRunList.sh
+++ launchPlugin/submitRunList.sh
@@ -4,13 +4,13 @@ PERIOD="2017_01-ver03"
 # PERIOD="2018_01-ver02"
 # PERIOD="2018_08-ver02"
 
-RUN_LIST="${PERIOD}.part.runList"
+RUN_LIST="../data/RD/${PERIOD}/${PERIOD}.part.runList"
 
-swif2 create ReactionEfficiency_RD_${PERIOD}
+swif2 create "ReactionEfficiency_RD_${PERIOD}"
 
 while read RUN_NMB
 do
-  ./launch.orig.py --verbose=True jobs_ReactionEfficiency_RD.config "${RUN_NMB}" "${RUN_NMB}" &> "ReactionEfficiency_RD_${PERIOD}_${RUN_NMB}.log"
+  "${HD_UTILITIES_HOME}/launch_scripts/launch/launch.py" --verbose=True ./jobs_ReactionEfficiency_RD.config "${RUN_NMB}" "${RUN_NMB}" &> "ReactionEfficiency_RD_${PERIOD}_${RUN_NMB}.log"
 done <${RUN_LIST}
 
-swif2 run -workflow ReactionEfficiency_RD_${PERIOD}
+swif2 run -workflow "ReactionEfficiency_RD_${PERIOD}"
